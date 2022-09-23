5772cb140dcf7989286c75eb0afcc801249c4b08
Nicolas Miller <njmiller1208@gmail.com>
2022-09-23 12:28:01 -0400

    Added support for word lists of arbitrary size.

--- sestina.py
+++ sestina.py
@@ -1,19 +1,20 @@
 # Program takes a list of words to be used as the end-words in a sestina and generates their order throughout
-# all seven stanzas of the poem
-words = ["whine", "shadow", "blast", "tunnel", "rest", "letter"]
-stanza_dict = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
-
-
+# all stanzas of the poem. While a sestina traditionally has only six words, this program can apply the sestina
+# permutation algorithm to word lists of arbitrary size.
 def permutate(sestina_words):
-    # The algorithm for the permutation has a pattern where we first subtract by 5, then add by 4, then subtract by 3,
-    # then add by 2, and finally subtract by 1.
-    temp_words = [0, 0, 0, 0, 0, 0]
+    # The algorithm for the permutation of the traditional sestina has a pattern where we first subtract by 5,
+    # then add by 4, then subtract by 3, then add by 2, and finally subtract by 1. Applying this method to the length
+    # of the initial list of words allows us to perform this permutation for any number of words.
+
+    # Creates temporary list for permutation of the length of the passed list
+    temp_words = [None] * len(sestina_words)
 
     # Stores the index of the element of sestina_list that will next be placed in temp_words.
-    location = 5
+    location = len(sestina_words) - 1
 
     # Used to indicate the amount by which we add or subtract for any given index and is decremented after each pass.
-    scaler = 5
+    # Begins as equal to the total number of words passed to the permutation function.
+    scaler = len(sestina_words) - 1
 
     for num in range(0, len(temp_words)):
         # Even indices will occur when we are subtracting, and odd indices occur when we add.
@@ -30,21 +31,45 @@ def permutate(sestina_words):
     return temp_words
 
 
-def sestina_me(six_words, final_dict, count=1):
-    if count == 1:
-        final_dict[count] = six_words
+def sestina_me(words, final_dict=None, count=1):
+    # Needs better error handling, but it works for current needs.
+    if len(words) <= 2:
+        # print("ERROR: The word count must be greater than two words.")
+        return "ERROR: The word count must be greater than two words."
+
+    if final_dict is None and count == 1:
+        # Initializes dictionary to store word order by stanza number
+        final_dict = {}
+        for i in range(1, len(words) + 1):
+            final_dict[i] = ''
+
+        # Assigns the original word order to the first stanza
+        final_dict[count] = words
         count += 1
-        sestina_me(six_words, final_dict, count)
+        sestina_me(words, final_dict, count)
 
     # Stopping condition
-    elif final_dict[6]:
-        # Final stanza consists of only three lines, so for simplicity, we'll handle manually
-        final_dict[7] = [six_words[3], six_words[4], six_words[5]]
+    elif final_dict[len(words)]:
+        # Final stanza should contain half of the lines of the original stanza size (a standard 6 line stanza sestina
+        # has a final 3 line stanza). The below code halves the total number of lines. If the number of words is odd,
+        # then 1 is added to the final value to be used if desired by the writer.
+        i = int(len(words) / 2)
+
+        if len(words) % 2 != 0:
+            i += 1
+
+        final_stanza = []
+
+        for num in range(-i, 0):
+            final_stanza.append(words[len(words) + num])
+
+        final_dict[len(words) + 1] = final_stanza
+
         return final_dict
 
-    # Extremely iterative recursive solution
+    # Permutates word order and recursively passes new permutation
     else:
-        new_perm = permutate(six_words)
+        new_perm = permutate(words)
         final_dict[count] = new_perm
         count += 1
         sestina_me(new_perm, final_dict, count)
@@ -52,9 +77,44 @@ def sestina_me(six_words, final_dict, count=1):
     return final_dict
 
 
-final_form = sestina_me(words, stanza_dict)
+test_words = ["whine", "shadow", "blast", "tunnel", "rest", "letter"]
+final_form = sestina_me(test_words)
 
-# Prints list of stanzas
+big_test_words = ["test", "this", "input", "on", "something", "bigger", "than", "a", "standard", "sestina"]
+big_final_form = sestina_me(big_test_words)
+
+small_test_words = ["only", "three", "words"]
+small_final_form = sestina_me(small_test_words)
+
+error_words = ["one", "two"]
+error_final_form = sestina_me(error_words)
+
+# Printing results of standard sestina with six lines
+print("Standard sestina with six lines:")
 for stanza in final_form:
     print("Stanza ", stanza)
     print(final_form[stanza])
+
+print()
+
+# Printing results of larger sestina with ten lines
+print("Larger sestina with ten lines:")
+for stanza in big_final_form:
+    print("Stanza ", stanza)
+    print(big_final_form[stanza])
+
+print()
+
+# Printing results of smaller sestina with two lines
+print("Smaller sestina with three lines:")
+for stanza in small_final_form:
+    print("Stanza ", stanza)
+    print(small_final_form[stanza])
+
+print()
+
+print("The result of passing two or fewer words: ")
+print(error_final_form)
+
+
+
